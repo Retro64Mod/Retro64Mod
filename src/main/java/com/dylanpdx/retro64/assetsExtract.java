846b8450fdfbd0b19653811f89748978b1ceecc8
NorthWestWind <wsyn148@gmail.com>
2022-04-05 23:49:08 +0800

    Support for Linux and Wine

--- src/main/java/com/dylanpdx/retro64/assetsExtract.java
+++ src/main/java/com/dylanpdx/retro64/assetsExtract.java
@@ -1,9 +1,13 @@
 package com.dylanpdx.retro64;
 
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.SystemUtils;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 /**
  * Extract audio assets
@@ -12,13 +16,18 @@ public class assetsExtract {
 
     public static String extractToTmp(File rom) throws IOException, InterruptedException {
         var tempDir = Files.createTempDirectory("retro64");
+        if (SystemUtils.IS_OS_WINDOWS) return windows(tempDir, rom);
+        else if (SystemUtils.IS_OS_LINUX) return linux(tempDir, rom);
+        // what do we do with mac?
+        else return null;
+    }
+
+    private static String windows(Path tempDir, File rom) throws IOException, InterruptedException {
         File exeFile = Path.of("mods","genfiles.exe").toFile();
-        // check if exe exists
         if (!exeFile.exists()) {
             Retro64.LOGGER.info("exe file not found");
             return null;
         }
-        // run exe with rom as 1st argument, and temp dir as working dir\
         ProcessBuilder pb = new ProcessBuilder(exeFile.getAbsolutePath(), rom.getAbsolutePath());
         pb.directory(tempDir.toFile());
         Process p = pb.start();
@@ -29,4 +38,28 @@ public class assetsExtract {
         return tempDir.toString();
     }
 
+    private static String linux(Path tempDir, File rom) throws IOException, InterruptedException {
+        // theres probably a better method to check for wine
+        // wine is tested and works flawlessly btw
+        boolean useWine = false;
+        File exeFile;
+        if (new File("/usr/bin/wine").exists() || new File("/usr/local/bin/wine").exists()) {
+            exeFile = Path.of("mods", "genfiles.exe").toFile();
+            useWine = true;
+        } else exeFile = Path.of("mods", "genfiles.sh").toFile();
+        if (!exeFile.exists()) {
+            Retro64.LOGGER.info("exe file not found");
+            return null;
+        }
+        List<String> commands = Lists.newArrayList(exeFile.getAbsolutePath(), rom.getAbsolutePath());
+        if (useWine) commands.add(0, "wine");
+        ProcessBuilder pb = new ProcessBuilder(commands);
+        pb.inheritIO().directory(tempDir.toFile());
+        Process p = pb.start();
+        p.waitFor();
+        // return all files in temp dir
+        if (tempDir.toFile().listFiles().length!=4)
+            return null;
+        return tempDir.toString();
+    }
 }
