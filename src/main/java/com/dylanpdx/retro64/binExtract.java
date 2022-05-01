b798e46ca404b1162e788239c589c457d9b526ce
Dylan <dylanpdx@gmail.com>
2022-05-01 23:32:17 +0100

    Extract locatiion changed to temporary directory

--- src/main/java/com/dylanpdx/retro64/binExtract.java
+++ src/main/java/com/dylanpdx/retro64/binExtract.java
@@ -1,13 +1,13 @@
 package com.dylanpdx.retro64;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.DefaultPlayerSkin;
 import net.minecraft.resources.ResourceLocation;
 import org.apache.commons.lang3.SystemUtils;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class binExtract {
     private static File genfiles_bin;
@@ -33,13 +33,14 @@ public class binExtract {
         Retro64.LOGGER.info("libsm64 location identified as "+libsm64Loc.toString());
         Retro64.LOGGER.info("genfiles location identified as "+genFilesExeLoc.toString());
         // extract into temp dir
+        var tempDir = Files.createTempDirectory("retro64_bin");
         var stream = Minecraft.getInstance().getResourceManager().getResource(libsm64Loc).getInputStream();
-        var libPath=Files.createTempFile("retro64_bins",libsm64);
+        var libPath=Path.of(tempDir.toString(),libsm64);
         Files.copy(stream, libPath, java.nio.file.StandardCopyOption.REPLACE_EXISTING);
         stream.close();
         Retro64.LOGGER.info("libsm64 extracted to "+libPath.toString());
         stream = Minecraft.getInstance().getResourceManager().getResource(genFilesExeLoc).getInputStream();
-        var genfilesPath=Files.createTempFile("retro64_bins",genFilesExe);
+        var genfilesPath=Path.of(tempDir.toString(),genFilesExe);
         Files.copy(stream, genfilesPath,java.nio.file.StandardCopyOption.REPLACE_EXISTING);
         stream.close();
         Retro64.LOGGER.info("genfiles extracted to "+genfilesPath.toString());
