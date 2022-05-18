df4781229006eaeccd2ce7c5608b734a0c74a16d
Dylan <dylanpdx@gmail.com>
2022-05-18 15:21:33 +0100

    Sonic model added

--- src/main/java/com/dylanpdx/retro64/ModelData.java
+++ src/main/java/com/dylanpdx/retro64/ModelData.java
@@ -2,16 +2,25 @@ package com.dylanpdx.retro64;
 
 public enum ModelData {
     MCHAR("","",0),
-    LUIGI("Luigi","Twitter: @FluffaLuigi",1),
+    LUIGI("Luigi","Twitter: @FluffaLuigi","https://twitter.com/FluffaLuigi",1),
     STEVE("Steve","Twitter: @sanikkat",2),
     ALEX("Alex","Twitter: @sanikkat",3),
-    NECOARC("Neco-Arc","Sketchfab: @paperbandit",4),
-    VIBRI("Vibri","Models Resource: @Ratinight",5);
+    NECOARC("Neco-Arc","Sketchfab: @paperbandit","https://sketchfab.com/3d-models/neco-arc-8bcd385adec44fdf8ebfc63bcdf5b28c",4),
+    VIBRI("Vibri","Models Resource: @Ratinight","https://www.models-resource.com/custom_edited/vibribboncustoms/model/48606/",5),
+    SONIC("Classic Sonic","sm64pc.info: @kurobutt","https://sm64pc.info/forum/viewtopic.php?f=2&t=141",6);
 
     ModelData(String name,String credit,int index){
         this.name = name;
         this.credit = credit;
         this.index = index;
+        this.creditURL=null;
+    }
+
+    ModelData(String name,String credit,String creditURL,int index){
+        this.name = name;
+        this.credit = credit;
+        this.index = index;
+        this.creditURL=creditURL; // currently unused, will be used for making credit clickable later
     }
 
     public String getName(){
@@ -28,6 +37,7 @@ public enum ModelData {
 
     private final String name;
     private final String credit;
+    private final String creditURL;
     private final int index;
     public static final int modelCount=6;
 }
