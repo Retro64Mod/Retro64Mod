3c361eac6b4108a92919786dec58ca7fe145e1ab
Dylan <dylanpdx@gmail.com>
2022-06-09 00:00:20 +0100

    1.19 Runs - Capabilities sometimes do not get set (Forge issue?)

--- src/main/java/com/dylanpdx/retro64/Utils.java
+++ src/main/java/com/dylanpdx/retro64/Utils.java
@@ -8,16 +8,21 @@ import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class Utils {
 
@@ -77,7 +82,7 @@ public class Utils {
      * @param random Random number generator
      * @return List of Vec3 vertices
      */
-    public static List<Vec3> getAllQuads(BakedModel bakedModel, BlockState blockState, Random random){
+    public static List<Vec3> getAllQuads(BakedModel bakedModel, BlockState blockState, RandomSource random){
         ArrayList<Vec3> quads = new ArrayList<>();
         quads.addAll(decodeQuads(bakedModel.getQuads(blockState, null,random)));
         quads.addAll(decodeQuads(bakedModel.getQuads(blockState, Direction.DOWN,random)));
@@ -215,5 +220,21 @@ public class Utils {
         //quaternion.z = 0.5f * num5;
         //quaternion.w = (m01 - m10) * num2;
         return quaternion;
+    }
+
+    public static String getRegistryName(Block block){
+        return ForgeRegistries.BLOCKS.getKey(block).toString();
+    }
+
+    public static String getRegistryName(Item item){
+        return ForgeRegistries.ITEMS.getKey(item).toString();
+    }
+
+    public static String getRegistryName(MobEffect effect){
+        return ForgeRegistries.MOB_EFFECTS.getKey(effect).toString();
+    }
+
+    public static String getRegistryName(SoundEvent sound){
+        return ForgeRegistries.SOUND_EVENTS.getKey(sound).toString();
     }
 }
